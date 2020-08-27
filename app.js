1ee17c22d53083f107795f996f30e0130e0d6317
mohamedelkatary <mohammed_elkatary@hotmail.com>
2020-08-27 15:39:29 -0600

    add

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.get('/', function(req,res){
 app.get('/about', function(req,res){
     res.sendFile(__dirname + '/about.html')
 });
+app.listen(3000,function(req,res){
+    console.log('server is running...')
+})
