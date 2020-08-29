47e80e980d06d9079663c9cb839a149291822f28
mohamedelkatary <mohammed_elkatary@hotmail.com>
2020-08-29 14:52:50 -0600

    add

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Shopping-Cart' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about');
-});
+
 module.exports = router;
